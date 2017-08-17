568c84c5705e00d62de67399c6415c72b46b18a4
Vamshi Pasunuru <vamship@outlook.com>
2017-08-17 17:27:42 +0530

    new:usr:installer script for EMR (#3)
    create req directories, copy rubix jars and change presto filesystem impl

--- rubix_admin/installer.py
+++ rubix_admin/installer.py
@@ -1,10 +1,11 @@
 import logging
 
 from fabric.operations import sudo, put, os
+from fabric.contrib.files import append
 from fabric.state import env
 from fabric.tasks import execute
 from fabric.utils import abort
-
+from fabric.api import settings
 
 class Installer:
     @classmethod
@@ -18,7 +19,7 @@ class Installer:
                                                        help="Install from RPM")
         cls.install_parser.add_argument("-r", "--rpm", required=True,
                                         help="Path to RPM file")
-        cls.install_parser.add_argument("-a", "--rpm-args", default="",
+        cls.install_parser.add_argument("-a", "--rpm-args", default="--ignoreos",
                                         help="Arguments to rpm command")
         cls.install_parser.set_defaults(func=cls.install_cmd)
 
@@ -31,6 +32,7 @@ class Installer:
     def install(cls, args):
         cls._scp(args)
         cls._rpm_install(args)
+        cls._rubix_op(args)
 
     @classmethod
     def _scp(cls, args):
@@ -50,7 +52,25 @@ class Installer:
 
     @classmethod
     def _rpm_install(cls, args):
-        return sudo('rpm -U %s %s' %
-                    (args.rpm_args,
-                     os.path.join(args.config["remote_packages_path"],
-                                  os.path.basename(args.rpm))))
+         sudo('rpm -U %s %s' %
+                (args.rpm_args,
+                 os.path.join(args.config["remote_packages_path"],
+                              os.path.basename(args.rpm))))
+
+    @classmethod
+    def _rubix_op(cls, args):
+        sudo("cp -a /usr/lib/rubix/lib/* /usr/lib/presto/lib/plugin/hive-hadoop2/")
+        sudo("cp -a /usr/lib/rubix/lib/* /usr/lib/hadoop/lib/")
+        sudo("mkdir -p /mnt/rubix/")
+        sudo("mkdir -p /var/lib/rubix/cache")
+        with settings(warn_only=True):
+          sudo("ln -s /var/lib/rubix/cache /mnt/rubix/")
+        count = 0;
+        while count < 5:
+          sudo("mkdir -p /var/lib/rubix/cache/data%s" % count)
+          count += 1
+
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3n.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
+        append("/usr/lib/presto/etc/catalog/hive.properties","hive.fs.s3a.impl=com.qubole.rubix.presto.CachingPrestoS3FileSystem", True)
+        sudo("python /usr/lib/rubix/bin/configure.py")
